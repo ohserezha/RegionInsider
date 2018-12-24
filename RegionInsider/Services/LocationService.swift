6c83d91faaa5f49d69c299fe62fc14cb2e6abf74
Serhii Horielov <gssreg@gmail.com>
2018-12-25 01:24:18 +0200

    LocationService initial implementation
    setup capabilities, info.plist description

--- RegionInsider/Services/LocationService.swift
+++ RegionInsider/Services/LocationService.swift
@@ -6,4 +6,112 @@
 //  Copyright © 2018 Sergey G. All rights reserved.
 //
 
-import Foundation
+import CoreLocation
+
+protocol LocationServiceDelegate: class {
+  func locationService(_ service: LocationService, didChangeAuthorization status: CLAuthorizationStatus)
+  func locationService(_ service: LocationService, didUpdateLocation location: CLLocation)
+  func locationService(_ service: LocationService, didEnterRegion region: CLCircularRegion)
+  func locationService(_ service: LocationService, didExitRegion region: CLCircularRegion)
+  func showLocationDisabledAlert()
+}
+
+extension LocationServiceDelegate {
+  func locationService(_ service: LocationService, didChangeAuthorization status: CLAuthorizationStatus) {}
+  func locationService(_ service: LocationService, didUpdateLocation location: CLLocation) {}
+  func locationService(_ service: LocationService, didEnterRegion region: CLCircularRegion) {}
+  func locationService(_ service: LocationService, didExitRegion region: CLCircularRegion) {}
+  func showLocationDisabledAlert() {}
+}
+
+class LocationService: NSObject {
+  private let locationManager = CLLocationManager()
+  weak var delegate: LocationServiceDelegate?
+  
+  var location: CLLocation? {
+    return locationManager.location
+  }
+  
+  override init() {
+    super.init()
+    locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+    locationManager.delegate = self
+    startMonitoringLocation()
+  }
+  
+  func authorizationStatus() -> CLAuthorizationStatus {
+    return CLLocationManager.authorizationStatus()
+  }
+  
+  func requestAlwaysAuthorization() {
+    locationManager.requestAlwaysAuthorization()
+  }
+  
+  func requestWhenInUseAuthorization() {
+    locationManager.requestWhenInUseAuthorization()
+  }
+  
+  func startMonitoringLocation() {
+    locationManager.startUpdatingLocation()
+  }
+  
+  func stopMonitoringLocation() {
+    locationManager.stopUpdatingLocation()
+  }
+  
+  func startMonitoringRegions(_ regionList: [CLCircularRegion]) {
+    clearAllMonitoredRegions()
+    guard
+      CLLocationManager.isMonitoringAvailable(for: CLCircularRegion.self),
+      CLLocationManager.authorizationStatus() == .authorizedAlways else {
+        return
+    }
+    regionList.forEach { locationManager.startMonitoring(for: $0) }
+  }
+  
+  func showLocationDisabledAlert() {
+    delegate?.showLocationDisabledAlert()
+  }
+  
+  static func stopMonitoringRegions() {
+    let locationManager = CLLocationManager()
+    for region in locationManager.monitoredRegions {
+      locationManager.stopMonitoring(for: region)
+    }
+  }
+}
+
+private extension LocationService {
+  func clearAllMonitoredRegions() {
+    locationManager.monitoredRegions.forEach {
+      locationManager.stopMonitoring(for: $0)
+    }
+  }
+}
+
+extension LocationService: CLLocationManagerDelegate {
+  func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+    if status == .authorizedAlways {
+      locationManager.allowsBackgroundLocationUpdates = true
+    }
+    delegate?.locationService(self, didChangeAuthorization: status)
+  }
+  
+  func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+    if let location = locations.last {
+      delegate?.locationService(self, didUpdateLocation: location)
+    }
+  }
+  
+  func locationManager(_ manager: CLLocationManager, didEnterRegion region: CLRegion) {
+    if let region = region as? CLCircularRegion {
+      delegate?.locationService(self, didEnterRegion: region)
+    }
+  }
+  
+  func locationManager(_ manager: CLLocationManager, didExitRegion region: CLRegion) {
+    if let region = region as? CLCircularRegion {
+      delegate?.locationService(self, didExitRegion: region)
+    }
+  }
+}

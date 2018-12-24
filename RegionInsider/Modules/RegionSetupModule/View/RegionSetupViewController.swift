1920fc120aff28bc6d50ecce0f598d82130fffce
Serhii Horielov <gssreg@gmail.com>
2018-12-24 22:06:34 +0200

    bind RegionSetup storyboard with code

--- RegionInsider/Modules/RegionSetupModule/View/RegionSetupViewController.swift
+++ RegionInsider/Modules/RegionSetupModule/View/RegionSetupViewController.swift
@@ -13,18 +13,73 @@ protocol RegionSetupViewInput: AnyObject {
 }
 
 class RegionSetupViewController: UIViewController {
-  
+  // MARK: - Output
   var output: RegionSetupViewOutput!
   
+  // MARK: - Outlets
+  @IBOutlet private weak var geoRegionHeaderLabel: UILabel!
+  @IBOutlet private weak var latitudeLabel: UILabel!
+  @IBOutlet private weak var latitudeTextField: UITextField!
+  @IBOutlet private weak var longitudeLabel: UILabel!
+  @IBOutlet private weak var longitudeTextField: UITextField!
+  @IBOutlet private weak var regionRadiusLabel: UILabel!
+  @IBOutlet private weak var regionRadiusTextField: UITextField!
+  
+  @IBOutlet private weak var networkRegionHeaderLabel: UILabel!
+  @IBOutlet private weak var networkSSIDLabel: UILabel!
+  @IBOutlet private weak var networkSSIDTextField: UITextField!
+  
+  @IBOutlet private weak var addGeoRegionsButton: UIButton!
+  
+  // MARK: - Lifecycle
   override func viewDidLoad() {
     super.viewDidLoad()
-    
-    // Do any additional setup after loading the view.
+    configureUI()
+  }
+  
+  // MARK: - Actions
+  @IBAction private func didTapSaveButton(_ sender: Any) {
+    // output.saveButtonTapped()
   }
 }
 
 extension RegionSetupViewController: RegionSetupViewInput {
   func configure() {
     
+  }
+}
+
+extension RegionSetupViewController: UITextFieldDelegate {
+  
+}
+
+private extension RegionSetupViewController {
+  enum Constants {
+    static let labelFontSize: CGFloat = 12
+    static let headerFontSize: CGFloat = 14
+  }
+  
+  func configureUI() {
+    navigationItem.title = "Setup Regions"
+    
+    geoRegionHeaderLabel.text = "Geo Region"
+    geoRegionHeaderLabel.font = .systemFont(ofSize: Constants.headerFontSize)
+    
+    latitudeLabel.text = "center latitude"
+    latitudeLabel.font = .systemFont(ofSize: Constants.labelFontSize)
+    
+    longitudeLabel.text = "center longitude"
+    longitudeLabel.font = .systemFont(ofSize: Constants.labelFontSize)
+    
+    regionRadiusLabel.text = "radius"
+    regionRadiusLabel.font = .systemFont(ofSize: Constants.labelFontSize)
+    
+    networkRegionHeaderLabel.text = "Network Region"
+    networkRegionHeaderLabel.font = .systemFont(ofSize: Constants.headerFontSize)
+    
+    networkSSIDLabel.text = "Wi-Fi name"
+    networkSSIDLabel.font = .systemFont(ofSize: Constants.labelFontSize)
+    
+    addGeoRegionsButton.setTitle("Save Regions", for: .normal)
   }
 }
